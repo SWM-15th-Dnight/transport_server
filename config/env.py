a0d21e8245cdbfab3b234da023ce83d40ad3c070
inmonim <cat1083014@gmail.com>
2024-09-05 14:57:10 +0900

    fix : 환경에 따라 배포 환경 분화 및 DB 연결 정보 수정

--- config/env.py
+++ config/env.py
@@ -2,13 +2,21 @@ import os
 
 profile = os.environ.get("CALINIFY_TRANSPORT_SERVER_PROFILE")
 
-if profile in ["PROD", "DEV"]:
+if profile == "DEV":
 
-    DB_HOST = os.environ.get("CALINIFY_DATABASE_HOST")
-    DB_USERNAME = os.environ.get("CALINIFY_DATABASE_USERNAME")
-    DB_PASSWORD = os.environ.get("CALINIFY_DATABASE_PASSWORD")
-    DB_PORT = os.environ.get("CALINIFY_DATABASE_PORT")
-    DB_TABLE_NAME = os.environ.get("CALINIFY_DATABASE_TABLE_NAME")
+    DB_HOST = os.environ.get("CALINIFY_DEV_DATABASE_HOST")
+    DB_USERNAME = os.environ.get("CALINIFY_DEV_DATABASE_USERNAME")
+    DB_PASSWORD = os.environ.get("CALINIFY_DEV_DATABASE_PASSWORD")
+    DB_PORT = os.environ.get("CALINIFY_DEV_DATABASE_PORT")
+    DB_TABLE_NAME = os.environ.get("CALINIFY_DEV_DATABASE_TABLE_NAME")
+
+elif profile == "PROD":
+
+    DB_HOST = os.environ.get("CALINIFY_PROD_DATABASE_HOST")
+    DB_USERNAME = os.environ.get("CALINIFY_PROD_DATABASE_USERNAME")
+    DB_PASSWORD = os.environ.get("CALINIFY_PROD_DATABASE_PASSWORD")
+    DB_PORT = os.environ.get("CALINIFY_PROD_DATABASE_PORT")
+    DB_TABLE_NAME = os.environ.get("CALINIFY_PROD_DATABASE_TABLE_NAME")
 
 else:
     from dotenv import dotenv_values
