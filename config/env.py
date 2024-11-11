9125cc952ed55426d86099d30d7eb349492dc62c
inmonim <cat1083014@gmail.com>
2024-11-11 11:28:09 +0900

    fix : dev 환경 env

--- config/env.py
+++ config/env.py
@@ -2,8 +2,7 @@ import os
 
 profile = os.environ.get("CALINIFY_TRANSPORT_SERVER_PROFILE")
 
-if profile == "DEV" or profile == "PROD":
-
+if profile == "PROD":
     DB_HOST = os.environ.get("CALINIFY_PROD_DATABASE_HOST")
     DB_USERNAME = os.environ.get("CALINIFY_PROD_DATABASE_USERNAME")
     DB_PASSWORD = os.environ.get("CALINIFY_PROD_DATABASE_PASSWORD")
@@ -15,6 +14,18 @@ if profile == "DEV" or profile == "PROD":
     S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
     AWS_REGION = os.environ.get("AWS_REGION")
 
+elif profile == "DEV":
+    DB_HOST = os.environ.get("CALINIFY_DEV_DATABASE_HOST")
+    DB_USERNAME = os.environ.get("CALINIFY_DEV_DATABASE_USERNAME")
+    DB_PASSWORD = os.environ.get("CALINIFY_DEV_DATABASE_PASSWORD")
+    DB_PORT = os.environ.get("CALINIFY_DEV_DATABASE_PORT")
+    DB_TABLE_NAME = os.environ.get("CALINIFY_DEV_DATABASE_TABLE_NAME")
+    
+    S3_IAM_ACCESS_KEY = os.environ.get("S3_IAM_ACCESS_KEY")
+    S3_IAM_SECRET_KEY = os.environ.get("S3_IAM_SECRET_KEY")
+    S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+    AWS_REGION = os.environ.get("AWS_REGION")
+    
 else:
     from dotenv import dotenv_values
 
