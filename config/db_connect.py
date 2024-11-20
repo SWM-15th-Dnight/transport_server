be71d623c7886a62993d2803643aa681a16cc09d
inmonim <cat1083014@gmail.com>
2024-11-20 20:09:00 +0900

    fix : sqlalchemy engine pool_recycle

--- config/db_connect.py
+++ config/db_connect.py
@@ -5,7 +5,7 @@ from .env import DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME, DB_TABLE_NAME
 
 _url = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_TABLE_NAME}"
 
-_engine = create_engine(url=_url, echo=True)
+_engine = create_engine(url=_url, echo=True, pool_recycle=3600)
 
 _Session = sessionmaker(bind=_engine, autocommit=False, autoflush=True)
 
